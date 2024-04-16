acfb68f0b61db013c9436af3e3c5dcecd5f97494
chldn2 <relioken2030@gmail.com>
2024-04-16 10:28:12 +0900

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,17 @@
+export default function FirstPost() {
+  return (
+    <div>
+      <h1>First Post</h1>
+      <p>This is the first blog post.</p>
+      <img src="https://www.dogster.com/wp-content/uploads/2024/02/red-toy-poodle-dog-at-the-park_Mykhaylo_Kozelko_Shutterstock.jpg" alt="Description of the image" />
+      <p>A poodle is a breed of dog that is known for its distinctive curly or corded coat and its elegant appearance. Poodles come in various sizes, including standard, miniature, and toy, with the standard poodle being the largest and the toy poodle being the smallest. They are highly intelligent and trainable dogs, which has led to their popularity as both family pets and working dogs.
+
+Poodles have a long history and are believed to have originated in Germany, where they were originally bred as water retrievers for hunting waterfowl. Their curly coat served to insulate them from cold water while hunting. Over time, they gained popularity across Europe and eventually around the world.
+
+One of the most recognizable features of poodles is their distinctive grooming style, with the coat often trimmed into specific shapes and patterns. This grooming style not only enhances their appearance but also helps to manage their dense, curly fur.
+
+In addition to their role as companions, poodles are also used in various dog sports and activities, including obedience trials, agility competitions, and as therapy dogs. They are known for their loyalty, intelligence, and affectionate nature, making them beloved pets for many families.</p>
+
+    </div>
+  );
+}
